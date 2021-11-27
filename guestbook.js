631b674a7c862e4269c805caf8f6460c54de844d
verttisaurio1 <82918020+verttisaurio1@users.noreply.github.com>
2021-11-26 20:54:18 -0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e5c32eaa.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
